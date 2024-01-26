620b906408f8bfe12494bad652749fd8fbbb667e
Bobbie Syväsalmi <bsyvasal@student.hive.fi>
2024-01-27 01:10:23 +0200

    seperated the bultins to minishell_builtins and pipex_builtins

--- pipex_builtins.c
+++ pipex_builtins.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.c                                         :+:      :+:    :+:   */
+/*   pipex_builtins.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vshchuki <vshchuki@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: bsyvasal <bsyvasal@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 14:50:48 by vshchuki          #+#    #+#             */
-/*   Updated: 2024/01/26 17:44:43 by vshchuki         ###   ########.fr       */
+/*   Updated: 2024/01/27 01:09:41 by bsyvasal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-/* 
-cd()
+int	echo(char *cmd, int count)
+{
+	int	i;
+	int	n_flag;
 
-pwd()
-
-export()
-
-unset() */
-
-int	echo_n(char *str) {
-	printf("%s", str);
+	i = 1;
+	n_flag = 0;
+	if (count >= 2 && ft_strncmp(cmd[1], "-n", 3) == 0)
+		n_flag = i++;
+	while (cmd[i])
+		ft_printf("%s", cmd[i++]);
+	if (!n_flag)
+		ft_printf("\n");
 	return (1);
 }
 
@@ -56,81 +58,7 @@ int pwd()
 	return (1);
 }
 
-/**
- * @param return status
-*/
-void	exit_builtin(char *status)
-{
-	char	*status_converted;
-	int		status_n;
-	char	*error_msg;
-	char	*temp;
-	
-	if (status)
-	{
-		status_n = ft_atoi(status);
-		status_converted = ft_itoa(status_n);
-/* 		if (!status_converted) // this should be somehow handled in the outer functions up to where the builtins is called
-			return (0); */
-		if (status[0] == '+')
-			status = status + 1;
-		if (ft_strncmp(status, status_converted, ft_strlen(status_converted) + 1) == 0)
-		{
-			free(status_converted);
-			ft_putstr_fd("exit\n", 1);
-			exit((char)status_n);
-		}
-		else
-		{
-			free(status_converted);
-			error_msg = ft_strjoin("bvsh: exit: ", status);
-			temp = error_msg;
-			error_msg = ft_strjoin(temp, ": numeric argument required");
-			ft_putstr_fd(error_msg, 2);
-			free(temp);
-			free(error_msg);
-			exit(255);
-		}
-	}
-}
-/**
- * @return 1 for success, 0 for failure
-*/
-/* int	export_var(t_pipe *data, char *var)
-{
-	int i;
-	int len;
 
-	len = ft_strchr(var, '=') - var;
-	if (!len)
-		return (0);
-	i = 0;
-	printf("len: %d\n", len);
-	while(data->envp[i])
-	{
-		// data->envp[i] = ft_strdup("hello=hello");
-		if (!ft_strncmp(data->envp[i], var, len + 1))
-		{
-			printf("data->envp[i]: %s\n", data->envp[i]);
-			data->envp[i][0] = 'X';
-			data->envp[i][1] = 'X';
-			data->envp[i][2] = 'X';
-			data->envp[i][3] = 'X';
-			data->envp[i][4] = 'X';
-			printf("data->envp[i]: %s\n", data->envp[i]);
-			// free(data->envp[i]);
-			// data->envp[i] = ft_strdup(var);
-		}
-		i++;
-	}
-	return (1);
-} */
-/**
- * is_exported = 0 for env
- * is_exported = 1 for export without options or arguments
- * 
- * @return 1 for success
-*/
 int	print_env_variables(t_pipe *data, int is_exported)
 {
 	int	i;
@@ -145,42 +73,26 @@ int	print_env_variables(t_pipe *data, int is_exported)
 	}
 	return (1);
 }
-int builtins(char **cmd, t_pipe *data, int	is_parent)
+int child_builtins(char **cmd, t_pipe *data)
 {
 	int count;
 
 	count = get_string_array_size(cmd);
-	printf("count: %d\n", count);
-	if (!is_parent)
-	{
-		if (ft_strncmp(cmd[0], "export", 7) == 0 && count == 1)
-			return print_env_variables(data, 1);
-		if (ft_strncmp(cmd[0], "env", 4) == 0 && count == 1)
-			return print_env_variables(data, 0);
-		if (ft_strncmp(cmd[0], "history", 8) == 0 && count == 1)
-			return history();
-		// if (ft_strncmp(cmd[0], "export", 7) == 0 && count == 2)
-		// 	return export_var(&data, cmd[1]);
-		if (ft_strncmp(cmd[0], "echo", 5) == 0 && count >= 2 && ft_strncmp(cmd[1], "-n", 3) == 0)
-			return (echo_n(cmd[2]));
-		if (ft_strncmp(cmd[0], "pwd", 4) == 0 && pwd())
-			return (1);
-	}
-	else if (is_parent)
-	{
-		// printf("is_parent in builtins!\n");
-		// printf("cmd[0]: %s\n", cmd[0]);
-		// printf("ft_strncmp(cmd[0], \"exit\", 5): %d\n", ft_strncmp(cmd[0], "exit", 5));
-		if (ft_strncmp(cmd[0], "exit", 5) == 0 && count == 1)
-			exit(0);
-		if (ft_strncmp(cmd[0], "exit", 5) == 0 && count == 2)
-			exit_builtin(cmd[1]);
-		if (ft_strncmp(cmd[0], "exit", 5) == 0 && count > 2)
-		{
-			ft_putstr_fd("exit\nbvsh: exit: too many arguments\n", 2);
-			return (1);
-		}
-	}
-
+	if (ft_strncmp(cmd[0], "export", 7) == 0 && count == 1)
+		return (print_env_variables(data, 1));
+	if (ft_strncmp(cmd[0], "export", 7) == 0)
+		return (1);
+	if (ft_strncmp(cmd[0], "env", 4) == 0 && count == 1)
+		return (print_env_variables(data, 0));
+	if (ft_strncmp(cmd[0], "echo", 5) == 0)
+		return (echo(cmd, count));
+	if (ft_strncmp(cmd[0], "pwd", 4) == 0 && pwd())
+		return (1);
+	if (ft_strncmp(cmd[0], "exit", 5) == 0)
+		return (1);
+	if (ft_strncmp(cmd[0], "cd", 3) == 0)
+		return (1);
+	if (ft_strncmp(cmd[0], "unset", 6) == 0)
+		return (1);
 	return (0);
 }
